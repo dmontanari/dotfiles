e94702d321565926c6da162ed5ae1dbfbb48cbf7
Daniel Montanari <dmontanari@gmail.com>
2017-09-02 19:21:50 -0300

    Check if packages are not installed
    Was checking if the package are installed for debug porposes.

---
+++ README.md
@@ -0,0 +1,19 @@
+
+        My ~/dotfiles backup and restore system
+
+This is a work in progress
+
+Automatic instalation of my personal environment
+
+The file install.py will try to install the following applications:
+        * vim-tiny
+        * vim-common
+        * vim-gnome
+        * vim-gui-common
+        * vim-runtime
+        * vim-scripts
+
+
+Backup of .vimrc
+Dependencies: vim
+
--- install.py
+++ install.py
@@ -20,7 +20,7 @@ vim_dependencies = ['vim-tiny','vim-common','vim-gnome',
 cache = Cache()
 
 for vim_dep in vim_dependencies:
-    if cache[vim_dep].is_installed:
+    if not cache[vim_dep].is_installed:
         print('Vim dependency ', vim_dep, ' not found. Installing...')
         # TODO : Its possible to use apt module to install packages ?
         install_the_package = 'sudo apt-get install --yes ' + vim_dep
