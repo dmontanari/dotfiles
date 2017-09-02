dea1ac09523b97b19055e2ad1755ef41d918cf77
Daniel Montanari <dmontanari@gmail.com>
2017-09-02 20:14:05 -0300

    vim, vundle and plugins instalation

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 
-        My ~/dotfiles backup and restore system
+My ~/dotfiles backup and restore system
 
 This is a work in progress
 
 Automatic instalation of my personal environment
 
 The file install.py will try to install the following applications:
+
         * vim-tiny
+
         * vim-common
+
         * vim-gnome
+
         * vim-gui-common
+
         * vim-runtime
+
         * vim-scripts
 
 
 Backup of .vimrc
+
 Dependencies: vim
+
+
 
--- install.py
+++ install.py
@@ -15,7 +15,7 @@ from apt import Cache as Cache
 #
 
 vim_dependencies = ['vim-tiny','vim-common','vim-gnome',
-                    'vim-gui-common','vim-runtime','vim-scripts']
+                    'vim-gui-common','vim-runtime','vim-scripts', 'git']
 
 cache = Cache()
 
@@ -26,6 +26,35 @@ for vim_dep in vim_dependencies:
         install_the_package = 'sudo apt-get install --yes ' + vim_dep
         print('--> ', install_the_package)
         sys(install_the_package)
+
+# Check for vim directory
+vim_dir = os.getenviron['HOME']
+vim_dir += "/.vim/bundle/"
+
+if not os.path.isdir(vim_dir):
+    os.makedirs(vim_dir)
+else:
+    print("Directory ", vim_dir, " exist...")
+
+# Clone vundle repository
+print("Clonnign vundle repository...")
+clone_vundle  = "git clone https://github.com/VundleVim/Vundle.vim.git "
+clone_vundle += "~/.vim/bundle/Vundle.vim"
+sys(clone_vundle)
+
+import shutil
+print("Copying vimrc to .vimrf")
+shutil.copy("vimrc", "~/.vimrc")
+
+print("Installing plugins...")
+vim_install_plugins = "vim +PluginsInstall +qall"
+
+sys(vim_install_plugins)
+
+printf("Done.")
+
+
+
 
 
 
